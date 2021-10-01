b00a016110cba3870da484cc4b4a35b1c6666e16
MarioWWEI <91709146+MarioWWEI@users.noreply.github.com>
2021-10-01 10:17:47 +0700

    Update addTokens.js
    x10zxxz

--- global/addTokens.js
+++ global/addTokens.js
@@ -6,11 +6,11 @@ async function getName(authToken) {
 };
 
 async function addTokens() {
-    const add_tokens = prompt('How many tokens do you want to add to your account? (500 daily)');
+    const add_tokens = prompt('How many tokens do you want to add to your account? (5000 daily)');
     const myToken = localStorage.token.split('JWT ')[1];
 
-    if (add_tokens > 500) {
-        alert('You can add up to 500 tokens daily')
+    if (add_tokens > 5000) {
+        alert('You can add up to 5000 tokens daily')
     }
 
     fetch('https://api.blooket.com/api/users/addtokens', {
